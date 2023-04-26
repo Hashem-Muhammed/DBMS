318629f549503e3b919c0d7ddaf00bd760647a45
kareem magdy <kareemmagdy85888@gmail.com>
2023-04-26 18:04:55 +0200

    drop table faeture

--- sub_menu_files/drop_table.sh
+++ sub_menu_files/drop_table.sh
@@ -1,2 +1,32 @@
 #!/usr/bin/bash 
-echo "i am in drop table"
+ls 
+echo "enter name of table you want to drop"
+read DROP
+while true
+do
+if [[ -f $DROP ]]
+		then
+			echo "are you sure you want to to remove this table permanently?"
+			echo "y / n
+			"
+			read ANSWER
+			if test $ANSWER = "y
+			"
+				then
+					rm -r $DROP
+					echo "$DROP table deleted succfully"
+					break;
+			else 
+				break;
+			fi
+		
+else	
+		echo "this table does not exist
+		"
+		echo "enter name of table you want to drop
+		"
+		read DROP
+		continue;
+fi
+done
+$SHELL
