285ae0e330754a91c54f574d1c595e2a8d0263fc
Hashem <hashemuhammed@gmail.com>
2023-04-28 21:35:35 +0200

    Made the validation for the update feature

---
+++ sub_menu_files/test.sh
@@ -0,0 +1,11 @@
+#!/bin/usr/bash
+
+CO=2
+val="hashe"
+arr=(1,2)
+echo $z
+
+	awk -v="$arr[@]}" -v whaa="$z" -F: '{if($2 == "hashe") {
+	for i in ${arr[@]}
+	print $i;
+	} }' employee
--- sub_menu_files/update.sh
+++ sub_menu_files/update.sh
@@ -17,8 +17,8 @@ do
 done
 #array of names of fields
 names=( $(awk -F: '{if(NR == 1){for (i=1; i<=NF; i++ ) print $i} }' ./$TABL ))
-
-
+types=( $(awk -F: '{if(NR == 2){for (i=1; i<=NF; i++ ) print $i} }' $TABL) ) 		
+echo "${types[@]}"
 while true
 do
 	echo ""
@@ -44,10 +44,11 @@ do
 			if test $i -gt 0
 			then
 			# if the column is valid take value of where
-				echo "enter the value "
-				read W_VAL
+				
 				#retrive feild number of the where column
 				W_COL_N=( $(awk -v c=$W_COL -F: '{if(NR==1){for(i=1; i<=NF; i++){if(c == $i){print i;}}}}' ./$TABL)) 
+				echo "enter the value }"
+				read W_VAL				
 				break;	
 
 			fi	
@@ -80,13 +81,70 @@ do
 
 			if test $i -gt 0
 			then
-				echo "enter the value"
-				read VAL
 				#retrive the field number of set column 
 				COL_N=( $(awk -v c=$COL -F: '{if(NR==1){for(i=1; i<=NF; i++){if(c == $i){print i;}}}}' ./$TABL))
+				((index=$COL_N - 1))
+				echo "enter the value it should be ${types[$index]}"
+				read VAL
+				
+				
+	
+				
+				
+				if [ "${types[$index]}" = "str" ];
+	then
+		while true
+			do
+			if ! [[ $VAL =~ ^[a-zA-Z]+$ ]]
+				then
+				echo "Invalid value! It should be ${types[$index]}"
+				read VAL
+				continue;
+			fi
+			if [[ $index -eq 0 ]]
+				then
+				exist=( $(sed '1,2 d' $TABL | awk -F: '{print $1}'| 								grep $VAL) )
+				if ! [[ -z "$exist" ]]
+					then
+					echo "There is a row with the same value! please try different one."
+					read VAL
+					continue;
+				fi
+			fi	
+			break;	
+			done
+fi			
+if [ "${types[$index]}" = "int" ];
+	then
+		while true
+			do
+			if ! [[ $VAL =~ [0-9] ]]
+				then
+				echo "Invalid value! It should be ${types[$index]}"
+				read VAL
+				continue;
+			fi
+			if [[ $index -eq 0 ]]
+				then
+				exist=( $(sed '1,2 d' $TABL | awk -F: '{print $1}'| 								grep $VAL) )
+				if ! [[ -z "$exist" ]]
+					then
+					echo "There is a row with the same value! please try different one."
+					read VAL
+					continue;
+				fi
+			fi
+			break;
+			done
+fi
+
+				
+			
 				#retrive an array of current value/values of the set column 
 				VAL_F=( $(awk -v W_COL=$W_COL_N -v W_VAL=$W_VAL -v VAL=$VAL -v COL=$COL_N -F: '{if($W_COL == W_VAL){print $COL}}' ./$TABL))
-				
+				if ! [[ ${#VAL_F[@]} -gt 1 && $index -eq 0 ]]
+					then
+					
 				for ele in ${VAL_F[@]}
 				do
 #pass to the sed command the records that meet the where condtion then using sed to replace all values in VAL_F array with the value user entered
@@ -100,9 +158,13 @@ do
 				echo ""	
 				break;	
 			fi	
-
+		
+	echo " You are trying to update the primary key with the same value in many rows!"
+	break;
+	fi
 
 	else 
 		echo "this column does not exist enter valid chioce "
 	fi
+	
 done	
